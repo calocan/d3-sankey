9a6eed9a44be8d7b5ef7a8306997b1baec785b15
Andy Likuski <andy@likuski.org>
2020-11-18 20:34:25 +0100

    Added .js to local imports

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,10 @@
-export {default as sankey} from './sankey';
+export {default as sankey} from './sankey.js';
 export {
   nodeToFeature, projectBoundingBox, sankeyGenerator, sankeyGeospatialTranslate, translateNodeFeature,
   unprojectNode, resolveNodeStage, resolveLinkStage, makeLinkStages, resolveNodeName,
   propertyObj
-} from './sankeyHelpers';
+} from './sankeyHelpers.js';
 export {
   center as sankeyCenter, left as sankeyLeft, right as sankeyRight, justify as sankeyJustify,
   sankeyLinkHorizontal
-} from './sankeyHelpersAlign';
+} from './sankeyHelpersAlign.js';
